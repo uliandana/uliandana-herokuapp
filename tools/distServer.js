a65826586c577c4905b9e779fff5e96a75189457
Oka Uliandana <oka.uliandana@gmail.com>
2020-09-23 13:12:01 +0700

    Change distServer port

--- tools/distServer.js
+++ tools/distServer.js
@@ -5,7 +5,7 @@ import path from 'path';
 import { chalkProcessing } from './chalkConfig';
 
 const app = express();
-const port = 4000;
+const port = 80;
 
 app.use(gzipStatic('dist'));
 app.use(historyApiFallback());
